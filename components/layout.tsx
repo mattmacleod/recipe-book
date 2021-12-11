926876eabc70f9bfb070e76827439d8a87d36f83
Matthew MacLeod <matt@umm.io>
2021-12-11 12:57:47 +0000

    Initial commit and layout

---
+++ components/layout.tsx
@@ -0,0 +1,54 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from './layout.module.scss';
+
+import MealIcon from '/public/images/icons/meal.svg';
+
+interface Props {
+  title?: string;
+}
+
+const Layout = (props: React.PropsWithChildren<Props>) => (
+  <div className={ styles.container }>
+    <Head>
+      <title>Recipe Book{ props.title ? ` – ${ props.title }` : '' }</title>
+      <meta name='description' content='A collection of recipes.' />
+      <meta charSet='utf-8' />
+    </Head>
+
+    <header className={ styles.header }>
+      <Link href='/'>
+        <a className={ styles.logo }>
+          <MealIcon />
+        </a>
+      </Link>
+      <h1 className={ styles.title }>
+        Recipes.
+      </h1>
+    </header>
+
+    <main className={ styles.main }>
+      { props.children }
+    </main>
+
+    <footer className={ styles.footer }>
+      <div className={ styles.links }>
+        <Link href='/'>
+          Home
+        </Link>
+        <span className={ styles.sep }>|</span>
+        <Link href='/about'>
+          About
+        </Link>
+      </div>
+      <div className={ styles.credit }>
+        Built by <a href='https://github.com/mattmacleod' target='_blank' rel='noreferrer'>
+          @mattmacleod
+        </a>
+      </div>
+    </footer>
+  </div>
+);
+
+export default Layout;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          dimensions: false,
+        }
+      }],
+    });
+
+    return config;
+  },
 }
